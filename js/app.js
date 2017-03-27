d68c1d8a40ed4b9ce8be1eb02e71389a26d3d32e
Jason <Jason@Colins-MacBook-Pro.local>
2017-03-26 21:05:41 -1000

    Completed #Kam Bowl

--- js/app.js
+++ js/app.js
@@ -1,11 +1,12 @@
-/*You will need to refer to the index.html page to complete the exercises below. Using DOM, please make the necessary changes to the website.
-
+/*You will need to refer to the index.html page to complete the exercises below. Using DOM, please make the necessary changes to the website.fs
 Also don't forget to run your live-server for this assignment.*/
 
 //1. Kapiolani Coffee Shop
 /*Despite being the cat's meow, Kapiolani Coffee Shop's current rating is n/a. Let's keep the hungry fans happy by changing the current ranking to:
 
 Rating: 5 Stars*/
+var KapiolaniRating = document.getElementById("rating1");
+rating1.innerHTML = "5 Stars";
 
 
 //2. Sunrise Restaurant
@@ -13,8 +14,10 @@ Rating: 5 Stars*/
 
 Price: $$$
 Adress: 525 Kapahulu Ave, Honolulu, HI 96815*/
-
-
+var sunrisePrice = document.getElementById("price2");
+sunrisePrice.innerHTML = "$.99";
+var sunriseAddress = document.getElementById("addy2");
+sunriseAddress.innerHTML = "1234 Sunset Ave.";
 //3. Ramen Nakamura
 /*Literally, where did the time go? Ramen Nakamura is missing the hours section! This mistake is timeless.
 
@@ -23,7 +26,10 @@ You will need to create a new div element with an id of "hours3". Inside this di
 Hours: 6pm - 12am Sun - Sat
 
 Append this div element to the div id "text3"*/
-
+var nakRamen = document.createElement("div");
+nakRamen.id = "hours3";
+nakRamen.innerHTML = "Hours: 6pm - 12am Sun-Sat";
+text3.appendChild(nakRamen);
 
 //4. Kam Bowl
 /*Kam Bowl got no spare time to update their info and business is suffering! Help them regain their kingpin status again.
@@ -34,7 +40,10 @@ Price: $$$
 
 Append this element to the div with the id of 
 "text4"*/
-
+var kamBowlPrice = document.createElement("div");
+kamBowlPrice.id = "price4";
+kamBowlPrice.innerHTML = "Price: $$$";
+text4.appendChild(kamBowlPrice);
 /*Create a new div element with an id of "addy4". Inside this div element, give it the following info:
 
 Address: 1620 N. School St, Honolulu, HI 96817
